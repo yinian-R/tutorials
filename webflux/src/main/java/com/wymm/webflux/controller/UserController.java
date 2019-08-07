ec1523eaec80d91bf0c6b15ef7e4b2e1542cc5ad
yinian <huangruijin49@163.com>
2019-08-08 00:20:35 +0800

    扩展 JPA

--- webflux/src/main/java/com/wymm/webflux/controller/UserController.java
+++ webflux/src/main/java/com/wymm/webflux/controller/UserController.java
@@ -34,6 +34,51 @@ public class UserController {
                 .defaultIfEmpty(new ResponseEntity<>(HttpStatus.NOT_FOUND));
     }
 
+
+    /**
+     * 根据年龄查找用户
+     *
+     * @param start 开始年龄(不包含)
+     * @param end   结束年龄(不包含)
+     * @return 用户列表
+     */
+    @GetMapping("/age/{start}/{end}")
+    public Flux<User> findByAge(@PathVariable int start, @PathVariable int end) {
+        return this.userRepository.findByAgeBetween(start, end);
+    }
+
+    /**
+     * 根据年龄查找用户
+     *
+     * @param start 开始年龄
+     * @param end   结束年龄
+     * @return 用户列表
+     */
+    @GetMapping(value = "/stream/age/{start}/{end}", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<User> streamFindByAge(@PathVariable int start, @PathVariable int end) {
+        return this.userRepository.findByAgeBetween(start, end);
+    }
+
+    /**
+     * 得到20~30年龄的用户列表
+     *
+     * @return 20~30年龄的用户列表
+     */
+    @GetMapping("/old")
+    public Flux<User> oldUser() {
+        return this.userRepository.oldUser();
+    }
+
+    /**
+     * 得到20~30年龄的用户列表
+     *
+     * @return 20~30年龄的用户列表
+     */
+    @GetMapping(value = "/stream/old", produces = MediaType.TEXT_EVENT_STREAM_VALUE)
+    public Flux<User> streamOldUser() {
+        return this.userRepository.oldUser();
+    }
+
     @PostMapping
     public Mono<User> createUser(@RequestBody User user) {
         user.setId(null);
--- webflux/src/main/java/com/wymm/webflux/repository/UserRepository.java
+++ webflux/src/main/java/com/wymm/webflux/repository/UserRepository.java
@@ -1,7 +1,27 @@
 package com.wymm.webflux.repository;
 
 import com.wymm.webflux.domain.User;
+import org.springframework.data.mongodb.repository.Query;
 import org.springframework.data.mongodb.repository.ReactiveMongoRepository;
+import reactor.core.publisher.Flux;
 
 public interface UserRepository extends ReactiveMongoRepository<User, String> {
+    /**
+     * 根据年龄查找用户
+     *
+     * @param start 开始年龄(不包含)
+     * @param end   结束年龄(不包含)
+     * @return 用户列表
+     */
+    Flux<User> findByAgeBetween(int start, int end);
+
+    /**
+     * 得到20~30年龄的用户
+     *
+     * @return 20~30年龄的用户
+     */
+    @Query("{'age' : {'$gte' : 20, '$lte' : 30}}")
+    Flux<User> oldUser();
+
+
 }
