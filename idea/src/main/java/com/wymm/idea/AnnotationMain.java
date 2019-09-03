6e608aba4054539817e890b3cc8b09e3c546bc9f
yinian <2575058497@qq.com>
2019-09-03 16:42:51 +0800

    add code annotation

--- idea/src/main/java/com/wymm/idea/AnnotationMain.java
+++ idea/src/main/java/com/wymm/idea/AnnotationMain.java
@@ -3,13 +3,16 @@ package com.wymm.idea;
 import com.wymm.idea.annotation.Select;
 import com.wymm.idea.builder.MapperBuilder;
 
+import java.lang.annotation.Annotation;
+import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 
-public class Main {
-    public static void main(String[] args) throws NoSuchMethodException {
+public class AnnotationMain {
+    public static void main(String[] args) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException {
         MapperBuilder builder = new MapperBuilder();
         builder.select();
         
+        // 遍历所有的注解，获取属性
         Class build = MapperBuilder.class;
         for (Method method : build.getMethods()) {
             if (method.isAnnotationPresent(Select.class)) {
@@ -19,11 +22,16 @@ public class Main {
             }
         }
         
-        
+        // 获取具体的注解，获取属性
         Method selectMethod = MapperBuilder.class.getDeclaredMethod("select");
         if (selectMethod.isAnnotationPresent(Select.class)) {
             Select select = selectMethod.getAnnotation(Select.class);
             System.out.println("Select value: " + select.value());
         }
+        
+        // 获取具体的注解，获取属性（耦合低）
+        Annotation selectAnnotation = MapperBuilder.class.getDeclaredMethod("select").getAnnotation(Select.class);
+        String value = (String) selectAnnotation.getClass().getMethod("value").invoke(selectAnnotation);
+        System.out.println("value: " + value);
     }
 }
